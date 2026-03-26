1a97530857996df97489fe44b8e6607534b8863b
Seoukki <babibesar709@gmail.com>
2026-03-27 06:54:48 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,10 +2,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
-
   return res.status(200).json({
     store_name:    process.env.STORE_NAME    || 'SeonsPrems',
-    wa_number:     process.env.WA_NUMBER     || '',
+    wa_number:     process.env.WA_NUMBER     || '62895402466525',
     telegram_user: process.env.TELEGRAM_USER || '',
     ig_user:       process.env.IG_USER       || '',
   });
